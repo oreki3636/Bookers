dabb4659b14474988376e5dcbcf0e5056d89b021
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-04-09 08:25:18 +0000

    [finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,50 @@
 class BooksController < ApplicationController
-  def new
+
+
+  def create
+     @book=Book.new(book_params)
+    if @book.save
+      flash[:notice]="投稿が完了しました"
+     redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render:index
+    end
+
   end
 
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+
+    @book=Book.find(params[:id])
+  if @book.update(book_params)
+       flash[:notice]="編集が完了しました"
+       redirect_to book_path(book.id)
+     else
+       render:edit
+     end
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get '/books' => 'books#index'
-  get 'books/show'
-  get 'books/edit'
-  post 'books'=>'books#create'
+
+   resources :books
   get 'top'=>'homes#top'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
