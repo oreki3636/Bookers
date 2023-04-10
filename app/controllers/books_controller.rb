b44d14e22be194ea7e422673505f913693967ce7
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-04-10 08:02:53 +0000

    [finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   def create
      @book=Book.new(book_params)
     if @book.save
-      flash[:notice]="投稿が完了しました"
+      flash[:notice]="Book was successfully created."
      redirect_to book_path(@book.id)
     else
       @books=Book.all
@@ -29,18 +29,19 @@ class BooksController < ApplicationController
   def update
 
     @book=Book.find(params[:id])
-  if @book.update(book_params)
-       flash[:notice]="編集が完了しました"
-       redirect_to book_path(book.id)
-     else
-       render:edit
-     end
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
   end
 
   def destroy
     book=Book.find(params[:id])
     book.destroy
-    redirect_to'/books'
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
   end
 
   private
